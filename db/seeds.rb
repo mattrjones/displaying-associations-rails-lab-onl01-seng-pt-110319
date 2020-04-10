a69625a47625868f97958acec603553560407d94
Matthew Jones <matts3371@gmail.com>
2020-04-10 12:34:18 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name: "Rush")
+Song.create(title: "Tom Swayer", artist_id: 1)
